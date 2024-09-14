9d642f6893fff3847585021514770e7e249f63bb
Ming Chen <v.mingchen@gmail.com>
2024-09-14 08:27:31 -0400

    simplify

--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,11 @@ mod test {
 
     #[test]
     fn test_basic() {
-        assert_eq!(
-            unsafe {
-                xdr_void(
-                    std::ptr::null_mut::<rpc_xdr>(),
-                    std::ptr::null_mut::<std::os::raw::c_void>(),
-                )
-            },
-            true
-        );
+        assert!(unsafe {
+            xdr_void(
+                std::ptr::null_mut::<rpc_xdr>(),
+                std::ptr::null_mut::<std::os::raw::c_void>(),
+            )
+        });
     }
 }
